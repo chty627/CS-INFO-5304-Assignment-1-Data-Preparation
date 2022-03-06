9c55c6cd3c30c2fe231de8c6e6650a89e119e279
Ning <45711839+chty627@users.noreply.github.com>
2022-03-06 01:00:10 -0500

    implemented question1

--- README.md
+++ README.md
@@ -5,6 +5,104 @@ Chenran Ning (cn257)
 This project is a coure project in `Data Science in the wild` from Cornell Tech.
 ## Question 1: Extract, Transform Load (ETL) (34 points)
 
+### Step 1: Load data (3 points)
+
+```python
+spark_df = spark.read.load(filepath, format=format)
+```
+
+### Step 2: Make test example (6 points)
+
+`brfss`and`nhis`files:
+
+```json
+{"SEX":2.0,"_LLCPWT":1,"_AGEG5YR":11.0,"_IMPRACE":1.0}
+{"SEX":1.0,"_LLCPWT":2,"_AGEG5YR":6.0,"_IMPRACE":1.0}
+{"SEX":2.0,"_LLCPWT":2,"_AGEG5YR":10.0,"_IMPRACE":5.0}
+{"SEX":2.0,"_LLCPWT":1,"_AGEG5YR":1.0,"_IMPRACE":1.0}
+{"SEX":2.0,"_LLCPWT":1,"_AGEG5YR":11.0,"_IMPRACE":1.0}
+```
+
+```csv
+SEX,MRACBPI2,HISPAN_I,AGE_P,DIBEV1
+,1,12,65,2
+2,1,12,19,2
+1,1,12,45,1
+2,1,0,67,1
+1,1,12,,2
+```
+
+```
+{"SEX":2.0,"_LLCPWT":1,"_AGEG5YR":1.0,"_IMPRACE":1.0}
+2,1,12,19,2
+{"SEX":1.0,"_LLCPWT":2,"_AGEG5YR":6.0,"_IMPRACE":1.0}
+1,1,12,45,1
+{"SEX":2.0,"_LLCPWT":2,"_AGEG5YR":10.0,"_IMPRACE":5.0}
+2,1,0,67,1
+
+```
+
+The joint set is :
+
+```
+SEX,_LLCPWT,_AGEG5YR,_IMPRACE,DIBEV1
+2.0,1,1.0,1.0,2
+1.0,2,6.0,1.0,1
+2.0,2,10.0,5.0,1
+
+```
+
+In file `brfss`:
+
+- SEX
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201358261.png" alt="image-20220305201358261" style="zoom: 33%;" />
+
+- _LLCPWT
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201641044.png" alt="image-20220305201641044" style="zoom:33%;" />
+
+- _AGEG5YR
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201715857.png" alt="image-20220305201715857" style="zoom:50%;" />
+
+- _IMPRACE
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201737400.png" alt="image-20220305201737400" style="zoom:50%;" />
+
+In `nhis` file:
+
+- SEX
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201835667.png" alt="image-20220305201835667" style="zoom:33%;" />
+
+- MRACBPI2
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305202016632.png" alt="image-20220305202016632" style="zoom:50%;" />
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305201737400.png" alt="image-20220305201737400" style="zoom:50%;" />
+
+- HISPAN_I
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305202054736.png" alt="image-20220305202054736" style="zoom:50%;" />
+
+- AGE_P
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305202121575.png" alt="image-20220305202121575" style="zoom:50%;" />
+
+- DIBEV1
+
+<img src="/Users/chty627/Desktop/Cornell/Second Term/DS in the wild/assignment 1/CS-INFO-5304-Assignment-1-Data-Preparation/${img}/image-20220305202211101.png" alt="image-20220305202211101" style="zoom:50%;" />
+
+### Step 3: Map data (12 points) 
+
+```python
+spark-submit p1.py test_brfss.json test_nhis.csv no
+spark-submit p1.py brfss_input.json nhis_input.csv no
+```
+
+
+
 ## Question 2: Dealing with messy and missing data (40 points)
 
 ## Question 3: Outlier Detection (10 points)
--- assignment1_p1_data/p1.py
+++ assignment1_p1_data/p1.py
@@ -1,5 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
 #feel free to def new functions if you need
 
 def create_dataframe(filepath, format, spark):
@@ -14,9 +15,9 @@ def create_dataframe(filepath, format, spark):
     """
 
     #add your code here
-    spark_df = None #temporary placeholder
-
+    spark_df = spark.read.load(filepath, format=format,header="true" )
     return spark_df
+
 
 
 def transform_nhis_data(nhis_df):
@@ -28,8 +29,32 @@ def transform_nhis_data(nhis_df):
     """
 
     #add your code here
-    transformed_df = None #temporary placeholder
     
+    df = nhis_df
+    # change name for 3 similar properties
+    # SEX,MRACBPI2,HISPAN_I,AGE_P,DIBEV1
+    df = df.withColumnRenamed("MRACBPI2","_IMPRACE").withColumnRenamed("AGE_P","_AGEG5YR")
+    df = df.withColumn("_IMPRACE", when(df._IMPRACE == 3 , 4) \
+        .when( (df._IMPRACE == 6) | (df._IMPRACE == 7) | (df._IMPRACE == 12) , 3) \
+        .when( (df._IMPRACE == 16) | (df._IMPRACE == 17) , 6) \
+        .when( (df.HISPAN_I != 12) , 5) \
+        .otherwise(df._IMPRACE))
+    df = df.withColumn("_AGEG5YR", when( (df._AGEG5YR >= 18) & (df._AGEG5YR <= 24), 1 ) \
+        .when( (df._AGEG5YR <= 29), 2 ) \
+        .when( (df._AGEG5YR <= 34), 3 ) \
+        .when( (df._AGEG5YR <= 39), 4 ) \
+        .when( (df._AGEG5YR <= 44), 5 ) \
+        .when( (df._AGEG5YR <= 49), 6 ) \
+        .when( (df._AGEG5YR <= 54), 7 ) \
+        .when( (df._AGEG5YR <= 59), 8 ) \
+        .when( (df._AGEG5YR <= 64), 9 ) \
+        .when( (df._AGEG5YR <= 69), 10 ) \
+        .when( (df._AGEG5YR <= 74), 11 ) \
+        .when( (df._AGEG5YR <= 79), 12 ) \
+        .when( (df._AGEG5YR <= 99), 13 ) \
+        .when( (df._AGEG5YR.isNotNull()), 14 ) \
+        .otherwise(df._AGEG5YR))
+    transformed_df = df.drop("HISPAN_I")
     return transformed_df
 
 
@@ -43,7 +68,39 @@ def calculate_statistics(joined_df):
     """
 
     #add your code here
-    pass
+    # Race and ethnic background 
+    # Gender 
+    # BRFSS categorical age
+    # SEX,_LLCPWT,_AGEG5YR,_IMPRACE, DIBEV1
+    df = joined_df
+    
+    with open('output.txt', 'w') as f:
+        # Race
+        races = {1 : "White, Non-Hispanic", 2 : "Black, Non-Hispanic", 3 : "Asian, Non-Hispanic",
+                4 : "American Indian/Alaskan Native, Non-Hispanic", 5 : "Hispanic", 6 : "Other race, Non-Hispanic"}
+        print("Prevalance in Race and ethnic background", file = f)
+        for key, value in races.items():
+            prevalance = df.filter((df._IMPRACE == key) & (df.DIBEV1 == 1)).count() / df.filter(df._IMPRACE == key).count() 
+            print(value + " : " + str(prevalance), file = f)
+
+        # Gender
+        male = df.filter((df.SEX == 1) & (df.DIBEV1 == 1)).count() / df.filter(df.SEX == 1).count()  
+        female = df.filter((df.SEX == 2) & (df.DIBEV1 == 1)).count() / df.filter(df.SEX == 2).count() 
+        print("\nPrevalance in Gender:", file = f)
+        print("Male: ", male , file = f)
+        print("Female: ", female, file = f)
+        
+        # BRFSS categorical age
+        ages = {1:"Age 18 to 24",2:"Age 25 to 29",3:"Age 30 to 34",4:"Age 35 to 39",5:"Age 40 to 44",
+                6:"Age 45 to 49",7:"Age 50 to 54",8:"Age 55 to 59",9:"Age 60 to 64",10:"Age 65 to 69",
+                11:"Age 70 to 74",12:"Age 75 to 79",13:"Age 80 or older"}
+        print("\nPrevalance in BRFSS categorical age", file = f)
+        for category, title in ages.items():
+            prevalance = df.filter((df._AGEG5YR == category) & (df.DIBEV1 == 1)).count() / df.filter(df._AGEG5YR == category).count() 
+            print(title, " : ", prevalance, file = f)
+            
+    return 
+    
 
 def join_data(brfss_df, nhis_df):
     """
@@ -55,8 +112,10 @@ def join_data(brfss_df, nhis_df):
 
     """
     #add your code here
-    joined_df = None ##temporary placeholder
-
+    # SEX,_LLCPWT,_AGEG5YR,_IMPRACE, DIBEV1
+    # SEX,MRACBPI2,HISPAN_I,AGE_P,DIBEV1
+    cond = [brfss_df.SEX == nhis_df.SEX, brfss_df._AGEG5YR == nhis_df._AGEG5YR, brfss_df._IMPRACE == nhis_df._IMPRACE ]
+    joined_df = brfss_df.join(nhis_df, cond).select(brfss_df.SEX, brfss_df._LLCPWT, brfss_df._AGEG5YR, brfss_df._IMPRACE, nhis_df.DIBEV1)
     return joined_df
 
 if __name__ == '__main__':
@@ -76,14 +135,14 @@ if __name__ == '__main__':
     # Load dataframes
     brfss_df = create_dataframe(brfss_filename, 'json', spark)
     nhis_df = create_dataframe(nhis_filename, 'csv', spark)
-    # Perform mapping on nhis dataframe
+    # # Perform mapping on nhis dataframe
     nhis_df = transform_nhis_data(nhis_df)
-    # Join brfss and nhis df
+    # # Join brfss and nhis df
     joined_df = join_data(brfss_df, nhis_df)
     # Save
     if save_output == "True":
         joined_df.write.csv(output_filename, mode='overwrite', header=True)
-    # Calculate and print statistics
+    # # Calculate and print statistics
     calculate_statistics(joined_df)
     
     
